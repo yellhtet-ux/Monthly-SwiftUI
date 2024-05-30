490a99b4a554cd2a608f52eb96a68af8f12ef8db
Andrew <andrewhardin@onenex.co>
2024-05-31 02:01:29 +0630

    initial commit

---
+++ Monthly-SwiftUI/App/Monthly_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monthly_SwiftUIApp.swift
+//  Monthly-SwiftUI
+//
+//  Created by Andrew Hardin on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Monthly_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Monthly-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Monthly-SwiftUI
+//
+//  Created by Andrew Hardin on 5/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
